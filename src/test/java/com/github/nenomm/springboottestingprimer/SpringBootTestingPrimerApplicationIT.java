4d902c85874e7b73395034a772a067c1c3eb8f04
Nenad Nenadovic <nenad.nenadovic@coinme.com>
2023-03-22 21:06:57 +0100

    add one dummy integration test

---
+++ src/test/java/com/github/nenomm/springboottestingprimer/SpringBootTestingPrimerApplicationIT.java
@@ -0,0 +1,16 @@
+package com.github.nenomm.springboottestingprimer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+class SpringBootTestingPrimerApplicationIT {
+
+  @Test
+  void dummyTest() {
+    System.out.println("Integration test is running...");
+    assertTrue(true);
+  }
+
+}
